8d6245806cd2d5c83e4539612b3bc3b64cececac
YeTinH <1609274650@qq.com>
2023-01-09 22:26:13 +0800

    first commit

---
+++ src/main/java/cc/ouki/GitTest.java
@@ -0,0 +1,8 @@
+package cc.ouki;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("11111111111111111");
+    }
+}
