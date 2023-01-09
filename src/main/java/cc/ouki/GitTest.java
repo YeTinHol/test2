e15152801d68287d7c278f6fe0f5e05da742a153
YeTinH <1609274650@qq.com>
2023-01-09 23:08:21 +0800

    Second commit

--- src/main/java/cc/ouki/GitTest.java
+++ src/main/java/cc/ouki/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("11111111111111111");
+        System.out.println("111111111");
+        System.out.println("222222222222222");
     }
 }
